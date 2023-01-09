b58cdfc049ab245cfcfe8d7ba8864877e6dec827
Frederico Carvalho <fredericocazevedo@gmail.com>
2023-01-09 17:44:27 -0300

    criacao do CategoriaModelo

--- finansys/src/app/in-memory-database.ts
+++ finansys/src/app/in-memory-database.ts
@@ -1,14 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { } from "./pages/categorias/shared/Categoria.model";
 
 export class InMemoryDatabase implements InMemoryDbService{
 
   createDb(){
     const categorias = [
-      { id: 1, name: 'Moradia', description: "Pagamentos de Contas da Casa" },
-      { id: 2, name: 'Saúde', description: "Plano de Saúde e Remédios" },
-      { id: 3, name: 'Lazer', description: "Cinema, parques, praia, etc" },
-      { id: 4, name: 'Salário', description: "Recebimento de salário" },
-      { id: 5, name: 'Freelas', description: "Trabalhos com freelancer" },
+      { id: 1, name: 'Moradia', descricao: "Pagamentos de Contas da Casa" },
+      { id: 2, name: 'Saúde', descricao: "Plano de Saúde e Remédios" },
+      { id: 3, name: 'Lazer', descricao: "Cinema, parques, praia, etc" },
+      { id: 4, name: 'Salário', descricao: "Recebimento de salário" },
+      { id: 5, name: 'Freelas', descricao: "Trabalhos com freelancer" },
     ];
     return {categorias}
   }
